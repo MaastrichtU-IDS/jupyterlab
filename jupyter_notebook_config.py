0cc41d4625b7cf50c9a97b5b0a1e3689fa2fa18a
Vincent Emonet <vincent.emonet@gmail.com>
2021-04-18 20:35:24 +0200

    add jupyter_notebook_config.py file to clone the repo at start

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,22 @@
+import os
+
+git_url = os.environ.get('GIT_URL')
+home_dir = os.environ.get('HOME')
+os.chdir(home_dir)
+
+if git_url:
+    repo_id = git_url.rsplit('/', 1)[-1]
+    os.system('git clone --quiet --recursive ' + git_url)
+    os.chdir(repo_id)
+
+    if os.path.exists('packages.txt'):
+        os.system('sudo apt-get update')
+        os.system('cat packages.txt | xargs sudo apt-get install -y')
+
+    if os.path.exists('requirements.txt'):
+        os.system('pip install -r requirements.txt')
+
+    if os.path.exists('extensions.txt'):
+        os.system('cat extensions.txt | xargs -I {} jupyter {} install --user')
+
+    os.chdir(home_dir)

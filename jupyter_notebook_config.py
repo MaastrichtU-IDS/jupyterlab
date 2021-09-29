f135495d4e5da5960760b9971314dc62ca2d6139
Vincent Emonet <vincent.emonet@gmail.com>
2021-09-30 00:15:58 +0200

    add separate folder for cuda gpu

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,10 +1,12 @@
 import os
 
 git_url = os.getenv('GIT_URL', None)
+git_name = os.getenv('GIT_NAME', 'Default user')
+git_email = os.getenv('GIT_EMAIL', 'default@maastrichtuniversity.nl')
 
-# home_dir = os.environ.get('HOME')
-# os.chdir(home_dir)
-# c.NotebookApp.terminado_settings = {'shell_command': ['/bin/zsh']}
+# Preconfigure git to avoid to do it manually
+os.system('git config --global user.name "' + git_name + '"')
+os.system('git config --global user.email "' + git_email + '"')
 
 if git_url:
     # repo_id = git_url.rsplit('/', 1)[-1].replace('.git', '')
@@ -21,3 +23,7 @@ if os.path.exists('requirements.txt'):
 
 if os.path.exists('extensions.txt'):
     os.system('cat extensions.txt | xargs -I {} jupyter {} install --user')
+
+# home_dir = os.environ.get('HOME')
+# os.chdir(home_dir)
+# c.NotebookApp.terminado_settings = {'shell_command': ['/bin/zsh']}
